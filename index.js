4ed3f31dd48057f1bd35d4433b06a06a61d63b4c
BlooAM <adam3200@vp.pl>
2022-03-12 16:33:22 +0100

    extend news endpoint to other sources

--- index.js
+++ index.js
@@ -5,27 +5,46 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+const newspapers = [
+    {
+        name: 'thetimes',
+        address: 'https://www.thetimes.co.uk/environment',
+    },
+    {
+        name: 'guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+    },
+    {
+        name: 'telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change',
+    },
+]
 const articles = []
 
-app.get('/', (req, res) => {
-    res.json('Welcome to my Climate Change News API')
-})
-
-app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
         .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
-
             $('a:contains("climate")', html).each(function () {
                 const title = $(this).text()
                 const url = $(this).attr('href')
                 articles.push({
                     title,
                     url,
+                    source: newspaper.name,
                 })  
-            })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+            })            
+        })
+
+})
+
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
 })
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
