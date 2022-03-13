7edad067b314ee60dfbe477c9f7684bc57e91b1f
BlooAM <adam3200@vp.pl>
2022-03-13 13:28:23 +0100

    endpoint for specific source

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const res = require('express/lib/response')
 
 const app = express()
 
@@ -50,4 +51,30 @@ app.get('/', (req, res) => {
 app.get('/news', (req, res) => {
     res.json(articles)
 })
+
+app.get('/news/:newspaperId', async (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].base
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("climate")', html).each(function() {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId,
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
